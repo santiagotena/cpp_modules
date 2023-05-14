1520b9677e91041d6eda0c4ba2fc83ca22c19c75
Santiago Tena Hernandez <stenahe@gmail.com>
2023-05-14 23:30:46 +0200

    Add base for main.cpp

--- cpp_00/ex01/main.cpp
+++ cpp_00/ex01/main.cpp
@@ -6,12 +6,34 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 16:30:24 by stena-he          #+#    #+#             */
-/*   Updated: 2023/05/11 16:36:23 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/05/14 23:25:51 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char **argv)
+#include <iostream>
+#include "Contact.hpp"
+#include "PhoneBook.hpp"
+
+int main(void)
 {
+	Phonebook	phonebook;
+	std::string	command;
+	
+	while (1) {
+		std::cout << "Enter a command: <ADD> <SEARCH> <EXIT>" << std::endl;
+		std::getline(std::cin, command);
+		if (command.compare("EXIT") == 0 || std::cin.eof())
+			return (0);
+		else if (command.compare("ADD") == 0)
+			phonebook.addContact();
+		else if (command.compare("SEARCH") == 0) {
+			return (0); ///Edit
+		} 
+		else {
+			std::cout << "Error: Invalid command" << std::endl;
+		}
+		
+	}
 
 	return (0);
 }
