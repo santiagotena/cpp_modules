a97860ee0ff44b038f39557175877acc3693ca48
Santiago Tena Hernandez <stenahe@gmail.com>
2023-05-14 23:29:46 +0200

    Add base for PhoneBook class

--- cpp_00/ex01/PhoneBook.cpp
+++ cpp_00/ex01/PhoneBook.cpp
@@ -6,7 +6,21 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 16:30:31 by stena-he          #+#    #+#             */
-/*   Updated: 2023/05/11 16:30:32 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/05/14 23:25:48 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PhoneBook.hpp"
+
+Phonebook::Phonebook(void) {
+	return ;
+}
+
+Phonebook::~Phonebook(void) {
+	return ;
+}
+
+int	Phonebook::addContact(void) {
+
+	return (0);
+}
--- cpp_00/ex01/PhoneBook.hpp
+++ cpp_00/ex01/PhoneBook.hpp
@@ -6,7 +6,30 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 18:52:06 by stena-he          #+#    #+#             */
-/*   Updated: 2023/05/11 18:52:07 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/05/14 23:25:44 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+
+# include "Contact.hpp"
+
+# define ERR_BLANK "Error: No empty fields allowed"
+
+class Phonebook {
+
+public:
+	Phonebook(void);
+	~Phonebook(void);
+
+	int	addContact(void);
+	
+private:
+	int		_index;
+	Contact	_contacts[8];
+
+	
+};
+
+#endif
