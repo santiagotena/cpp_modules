feeea46fe3d3e9791f53de9f495e9c7fc0bdba80
Santiago Tena Hernandez <stenahe@gmail.com>
2023-05-15 01:53:35 +0200

    Add addContact func

--- cpp_00/ex01/PhoneBook.cpp
+++ cpp_00/ex01/PhoneBook.cpp
@@ -6,21 +6,56 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 16:30:31 by stena-he          #+#    #+#             */
-/*   Updated: 2023/05/14 23:25:48 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/05/15 01:23:49 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
 Phonebook::Phonebook(void) {
+	this->_index = 0;
+	std::cout << "Phonebook created" << std::endl;
 	return ;
 }
 
 Phonebook::~Phonebook(void) {
+	std::cout << "Phonebook destroyed" << std::endl;
 	return ;
 }
 
-int	Phonebook::addContact(void) {
+int	Phonebook::addContact(void) {	
+	if (this->_index > 7)
+		this->_index = 0;
 
+	// Contact currentContact = this->_contacts[this->_index];
+
+	if (this->_contacts[this->_index].setFirstName() != 0)
+		return (-1);
+	if (this->_contacts[this->_index].setLastName() != 0)
+		return (-1);
+	if (this->_contacts[this->_index].setNickname() != 0)
+		return (-1);
+	if (this->_contacts[this->_index].setPhoneNumber() != 0)
+		return (-1);
+	if (this->_contacts[this->_index].setDarkestSecret() != 0)
+		return (-1);
+	
+	this->_index++;
 	return (0);
+}
+
+void	Phonebook::displayHeader(void) {
+	return ;
+}
+
+void	Phonebook::displayContactList(void) {
+	return ;
+}
+
+void	Phonebook::displayFooter(void) {
+	return ;
+}
+
+void	Phonebook::displayContact(int index) {
+	return ;
 }
