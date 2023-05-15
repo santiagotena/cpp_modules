20894096e9a84ca28852e0ab24504aa6ec0bddd2
Santiago Tena Hernandez <stenahe@gmail.com>
2023-05-15 02:55:10 +0200

    Expand file

--- cpp_00/ex01/PhoneBook.cpp
+++ cpp_00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 16:30:31 by stena-he          #+#    #+#             */
-/*   Updated: 2023/05/15 01:59:41 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/05/15 02:53:32 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ Phonebook::~Phonebook(void) {
 int	Phonebook::addContact(void) {	
 	Contact *currentContact;
 	
-	if (this->_index > 7)
+	if (this->_index > (BOOK_SIZE - 1))
 		this->_index = 0;
 	currentContact = &this->_contacts[this->_index];
 	if (currentContact->setFirstName() != 0)
@@ -44,17 +44,35 @@ int	Phonebook::addContact(void) {
 }
 
 void	Phonebook::displayHeader(void) {
+	std::cout << "|-------------------------------------------|" << std::endl;
+    std::cout << "|     Index|First Name| Last Name|  Nickname|" << std::endl;
+    std::cout << "|-------------------------------------------|" << std::endl;
+	return ;
+}
+
+void	adjustField(std::string field) {
+	return ;
+}
+
+void	Phonebook::displayContactInList(int index) {
+	// this->_contacts[index];
 	return ;
 }
 
 void	Phonebook::displayContactList(void) {
+	displayHeader();
+	for (int i = 0; i < this->_index; i++)
+	{
+		displayContactInList(i);
+		std::cout << "|-------------------------------------------|" << std::endl;
+	}
 	return ;
 }
 
-void	Phonebook::displayFooter(void) {
-	return ;
-}
-
-void	Phonebook::displayContact(int index) {
+void	Phonebook::displaySingleContact(std::string index) {
+	// Check if index is number
+	// Check if within bounds
+	// Check if field not empty
+	// Print contact
 	return ;
 }
