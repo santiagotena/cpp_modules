bf9b6a02039c336db57d4579f42e50ae51b8f43c
Santiago Tena Hernandez <stenahe@gmail.com>
2023-05-16 13:18:27 +0200

    Change variable names

--- cpp_01/ex00/newZombie.cpp
+++ cpp_01/ex00/newZombie.cpp
@@ -6,15 +6,15 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 23:24:06 by stena-he          #+#    #+#             */
-/*   Updated: 2023/05/16 12:48:26 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/05/16 13:17:17 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie *newZombie(std::string name) {
-	Zombie	*newZombie;
+	Zombie	*undead;
 
-	newZombie = new Zombie(name);
-	return (newZombie);
+	undead = new Zombie(name);
+	return (undead);
 }
