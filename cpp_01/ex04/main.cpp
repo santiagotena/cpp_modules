82c6a76e3931de99f368953ccba2ac9b4a3b3649
Santiago Tena Hernandez <stenahe@gmail.com>
2023-05-17 12:40:13 +0200

    Refactor code

--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -6,19 +6,41 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 10:16:04 by stena-he          #+#    #+#             */
-/*   Updated: 2023/05/17 11:41:40 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/05/17 12:39:49 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fstream>
 #include <iostream>
-// #include <string>
 
 #define ERR_ARGC 		"Error: Wrong number of arguments.\nProvide <filename> <string1> <string2>"
 #define	ERR_EMPTY_PARAM "Error: No empty parameters allowed"
 #define ERR_IN_FILE		"Error: In file error"
 #define ERR_OUT_FILE	"Error: Out file error"	
 
+void	createOutputFile(std::string fileName, std::string& content) {
+	fileName.append(".replace");
+	std::ofstream	outFile(fileName);
+	outFile << content;
+	outFile.close();
+	return ;
+}
+
+void	replaceInContent(	std::string& content, 
+							std::string oldString, 
+							std::string newString
+						) {
+	size_t index;
+	
+	index = content.find(oldString);
+	while (index != std::string::npos)
+	{
+		content.erase(index, oldString.length());
+		content.insert(index, newString);
+		index = content.find(oldString);
+	}
+	return ;
+}
 
 std::string	createContent(std::ifstream &inFile) {
 	std::string	output;
@@ -30,7 +52,40 @@ std::string	createContent(std::ifstream &inFile) {
 		output.append(buffer);
 		output.append("\n");
 	}
+	inFile.close();
 	return (output);
+}
+
+bool	isInFileValid(std::ifstream& inFile) {
+	
+	if (!inFile.is_open())
+	{
+		std::cout << ERR_IN_FILE << std::endl;
+		inFile.close();
+		return (-1);
+	}
+	
+	return (true);
+}
+
+bool	isInputValid(int argc, char **argv) {
+	std::string		fileName;
+	std::string		oldString;
+	std::string		newString;
+
+	if (argc != 4) {
+		std::cout << ERR_ARGC << std::endl;
+		return (false);
+	}
+	fileName = argv[1];
+	oldString = argv[2];
+	newString = argv[3];
+	if (fileName.empty() || oldString.empty() || newString.empty())
+	{
+		std::cout << ERR_EMPTY_PARAM << std::endl;
+		return (false);
+	}
+	return (true);
 }
 
 int	main(int argc, char **argv) {
@@ -40,40 +95,21 @@ int	main(int argc, char **argv) {
 	std::string		newString;
 	std::string		content;
 
-	if (argc != 4) {
-		std::cout << ERR_ARGC << std::endl;
+	if (!isInputValid(argc, argv))
 		return (-1);
-	}
 	fileName = argv[1];
 	oldString = argv[2];
 	newString = argv[3];
-	if (fileName.empty() || oldString.empty() || newString.empty())
-	{
-		std::cout << ERR_EMPTY_PARAM << std::endl;
-		return (-1);
-	}
+	
 	std::ifstream	inFile(fileName);
-	if (!inFile.is_open())
-	{
-		std::cout << ERR_IN_FILE << std::endl;
+	
+	if (!isInFileValid(inFile))
 		return (-1);
-	}
+	
 	content = createContent(inFile); 
 
-	size_t index;
-	index = content.find(oldString);
-	while (index != std::string::npos)
-	{
-		content.erase(index, oldString.length());
-		content.insert(index, newString);
-		index = content.find(oldString);
-	}
+	replaceInContent(content, oldString, newString);
 	
-	fileName.append(".replace");
-	std::ofstream	outFile(fileName);
-	outFile << content;
-
-	inFile.close();
-	outFile.close();
+	createOutputFile(fileName, content);
 	return (0);
 }
