bbc2440541bb6c38c9eb5f49a91314c2d7073133
Santiago Tena Hernandez <stenahe@gmail.com>
2023-05-19 16:05:08 +0200

    Comment out equality operator

--- cpp_02/ex00/main.cpp
+++ cpp_02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 23:27:49 by stena-he          #+#    #+#             */
-/*   Updated: 2023/05/19 15:50:51 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/05/19 16:03:33 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	main(void) {
 
 	c = b;
 
-	bool isEqual = (c == b);
-    std::cout << "Equality: " << (isEqual ? "true" : "false") << std::endl;
+	// bool isEqual = (c == b);
+    // std::cout << "Equality: " << (isEqual ? "true" : "false") << std::endl;
 
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
