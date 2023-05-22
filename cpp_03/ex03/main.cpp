744474589a6d8d5786690a33159b39c7c4c2050a
Santiago Tena Hernandez <stenahe@gmail.com>
2023-05-22 23:54:12 +0200

    Expand test

--- cpp_03/ex03/main.cpp
+++ cpp_03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 22:55:37 by stena-he          #+#    #+#             */
-/*   Updated: 2023/05/22 23:48:59 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/05/22 23:51:26 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,12 @@ int	main(void)
 		marie.attack("Jora");
 		marie.takeDamage(50);
 		marie.beRepaired(10);
+		marie.checkStatus();
 		marie.highFivesGuys();
 		marie.whoAmI();
 		marie.guardGate();
 		marie.takeDamage(50);
+		marie.checkStatus();
 		marie.takeDamage(50);
 		marie.attack("Jora");
 	}
