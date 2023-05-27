05b7c8d1070867588b1015a34681ac29affa4154
Santiago Tena Hernandez <stenahe@gmail.com>
2023-05-28 00:41:58 +0200

    Expand testing

--- cpp_04/ex03/main.cpp
+++ cpp_04/ex03/main.cpp
@@ -17,20 +17,124 @@
 
 int main()
 {
-    IMateriaSource* src = new MateriaSource();
-    src->learnMateria(new Ice());
-    src->learnMateria(new Cure());
-    ICharacter* me = new Character("me");
-    AMateria* tmp;
-    tmp = src->createMateria("ice");
-    me->equip(tmp);
-    tmp = src->createMateria("cure");
-    me->equip(tmp);
-    ICharacter* bob = new Character("bob");
-    me->use(0, *bob);
-    me->use(1, *bob);
-    delete bob;
-    delete me;
-    delete src;
+    std::string partition(50, '-');
+
+    std::cout << partition << std::endl;
+    std::cout << "// BASE TESTS //" << std::endl;
+    std::cout << partition << std::endl;
+    {
+        IMateriaSource* src = new MateriaSource();
+        src->learnMateria(new Ice());
+        src->learnMateria(new Cure());
+        ICharacter* me = new Character("me");
+        AMateria* tmp;
+        tmp = src->createMateria("ice");
+        me->equip(tmp);
+        tmp = src->createMateria("cure");
+        me->equip(tmp);
+        ICharacter* bob = new Character("bob");
+        me->use(0, *bob);
+        me->use(1, *bob);
+        delete bob;
+        delete me;
+        delete src;
+    }
+    std::cout << partition << std::endl;
+    std::cout << "// DISPLAY TESTS //" << std::endl;
+    std::cout << partition << std::endl;
+    {
+        Character zack("Zack");
+        MateriaSource *src = new MateriaSource();
+        src->learnMateria(new Ice());
+        src->learnMateria(new Cure());
+        src->displayMemory();
+        AMateria* tmp;
+        tmp = src->createMateria("ice");
+        zack.equip(tmp);
+        tmp = src->createMateria("cure");
+        zack.equip(tmp);
+        tmp = src->createMateria("ice");
+        zack.equip(tmp);
+        zack.displayInventory();
+        zack.displayStorage();
+        zack.unequip(2);
+        zack.displayInventory();
+        zack.displayStorage();
+        delete src;
+    }
+    std::cout << partition << std::endl;
+    std::cout << "// CLONE TESTS //" << std::endl;
+    std::cout << partition << std::endl;
+    {
+        Character soldier("Soldier");
+        MateriaSource *src = new MateriaSource();
+        src->learnMateria(new Ice());
+        src->learnMateria(new Cure());
+        src->displayMemory();
+        AMateria* tmp;
+        tmp = src->createMateria("ice");
+        soldier.equip(tmp);
+        tmp = src->createMateria("cure");
+        soldier.equip(tmp);
+        soldier.displayInventory();
+        soldier.displayStorage();
+
+        std::cout << "Soldier Copy Constructor Test:" << std::endl;
+        Character soldier2(soldier);
+        soldier2.unequip(1);
+        soldier.displayInventory();
+        soldier.displayStorage();
+        soldier2.displayInventory();
+        soldier2.displayStorage();
+
+        std::cout << "Soldier Copy Assignment Constructor Test:" << std::endl;
+        Character soldier3 = soldier;
+        tmp = src->createMateria("cure");
+        soldier3.equip(tmp);
+        soldier.displayInventory();
+        soldier.displayStorage();
+        soldier3.displayInventory();
+        soldier3.displayStorage();
+    }
+    std::cout << partition << std::endl;
+    std::cout << "// EQUIP NULL //" << std::endl;
+    std::cout << partition << std::endl;
+    {
+        Character tifa("Tifa");
+        MateriaSource *src = new MateriaSource();
+        src->learnMateria(new Ice());
+        src->displayMemory();
+        AMateria* tmp;
+        tmp = src->createMateria("ice");
+        tifa.equip(tmp);
+        tmp = src->createMateria("wrong");
+        tifa.equip(NULL);
+        tifa.displayInventory();
+        tifa.displayStorage();
+    }
+    std::cout << partition << std::endl;
+    std::cout << "// OVER-EQUIP //" << std::endl;
+    std::cout << partition << std::endl;
+    {
+        Character cloud("Cloud");
+        MateriaSource *src = new MateriaSource();
+        src->learnMateria(new Ice());
+        src->learnMateria(new Cure());
+        src->displayMemory();
+        AMateria* tmp;
+        tmp = src->createMateria("ice");
+        cloud.equip(tmp);
+        tmp = src->createMateria("cure");
+        cloud.equip(tmp);
+        tmp = src->createMateria("ice");
+        cloud.equip(tmp);
+        tmp = src->createMateria("cure");
+        cloud.equip(tmp);
+        tmp = src->createMateria("ice");
+        cloud.equip(tmp);
+        cloud.displayInventory();
+        cloud.displayStorage();
+    }
+    std::cout << partition << std::endl;
     return 0;
 }
