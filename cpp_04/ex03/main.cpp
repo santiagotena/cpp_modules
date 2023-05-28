9bfa2ad2ccfdc095838124040e6708d173575591
Santiago Tena Hernandez <stenahe@gmail.com>
2023-05-28 11:57:45 +0200

    Simplify testing, free memory

--- cpp_04/ex03/main.cpp
+++ cpp_04/ex03/main.cpp
@@ -44,23 +44,16 @@ int main()
     std::cout << partition << std::endl;
     {
         Character zack("Zack");
-        MateriaSource *src = new MateriaSource();
-        src->learnMateria(new Ice());
-        src->learnMateria(new Cure());
-        src->displayMemory();
-        AMateria* tmp;
-        tmp = src->createMateria("ice");
-        zack.equip(tmp);
-        tmp = src->createMateria("cure");
-        zack.equip(tmp);
-        tmp = src->createMateria("ice");
-        zack.equip(tmp);
+        Character aeris("Aeris");
+        zack.equip(new Ice());
+        zack.equip(new Cure);
+        zack.equip(new Ice());
         zack.displayInventory();
         zack.displayStorage();
         zack.unequip(2);
         zack.displayInventory();
         zack.displayStorage();
-        delete src;
+        zack.use(1, aeris);
     }
     std::cout << partition << std::endl;
     std::cout << "// CLONE TESTS //" << std::endl;
@@ -79,22 +72,30 @@ int main()
         soldier.displayInventory();
         soldier.displayStorage();
 
+        std::cout << std::endl;
         std::cout << "Soldier Copy Constructor Test:" << std::endl;
+        std::cout << partition << std::endl;
         Character soldier2(soldier);
         soldier2.unequip(1);
         soldier.displayInventory();
         soldier.displayStorage();
+        std::cout << std::endl;
         soldier2.displayInventory();
         soldier2.displayStorage();
 
+        std::cout << std::endl;
         std::cout << "Soldier Copy Assignment Constructor Test:" << std::endl;
+        std::cout << partition << std::endl;
         Character soldier3 = soldier;
         tmp = src->createMateria("cure");
         soldier3.equip(tmp);
         soldier.displayInventory();
         soldier.displayStorage();
+        std::cout << std::endl;
         soldier3.displayInventory();
         soldier3.displayStorage();
+
+        delete src;
     }
     std::cout << partition << std::endl;
     std::cout << "// EQUIP NULL //" << std::endl;
@@ -108,9 +109,11 @@ int main()
         tmp = src->createMateria("ice");
         tifa.equip(tmp);
         tmp = src->createMateria("wrong");
-        tifa.equip(NULL);
+        tifa.equip(tmp);
         tifa.displayInventory();
         tifa.displayStorage();
+
+        delete src;
     }
     std::cout << partition << std::endl;
     std::cout << "// OVER-EQUIP //" << std::endl;
@@ -134,6 +137,8 @@ int main()
         cloud.equip(tmp);
         cloud.displayInventory();
         cloud.displayStorage();
+
+        delete src;
     }
     std::cout << partition << std::endl;
     return 0;
