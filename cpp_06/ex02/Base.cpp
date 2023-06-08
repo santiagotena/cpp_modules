8f2f1327058b0364f8d99680c47efc70bdbf1f0d
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-08 09:19:52 +0200

    Improve solution

--- cpp_06/ex02/Base.cpp
+++ cpp_06/ex02/Base.cpp
@@ -26,34 +26,60 @@ Base    *generate() {
     {
         case 0:
             ptr = new A;
-            std::cout << "Type A is generated." << std::endl;
+            std::cout << "Type A is generated. Address: " << ptr << std::endl;
             break;
         case 1:
             ptr = new B;
-            std::cout << "Type B is generated." << std::endl;
+            std::cout << "Type B is generated. Address: " << ptr << std::endl;
             break;
         case 2:
             ptr = new C;
-            std::cout << "Type C is generated." << std::endl;
+            std::cout << "Type C is generated. Address: " << ptr << std::endl;
             break;
     }
     return (ptr);
 }
 
 void identify(Base *ptr) {
-    if (dynamic_cast<A*>(ptr) != NULL)
-        std::cout << "Type is A" << std::endl;
-    else if (dynamic_cast<B*>(ptr) != NULL)
-        std::cout << "Type is B" << std::endl;
-    else if (dynamic_cast<C*>(ptr) != NULL)
-        std::cout << "Type is C" << std::endl;
+    A   *Aptr = dynamic_cast<A *>(ptr);
+    B   *Bptr = dynamic_cast<B *>(ptr);
+    C   *Cptr = dynamic_cast<C *>(ptr);
+
+    if (Aptr)
+        std::cout << "Type is A. Address: " << Aptr << std::endl;
+    else if (Bptr)
+        std::cout << "Type is B. Address: " << Bptr << std::endl;
+    else if (Cptr)
+        std::cout << "Type is C. Address: " << Cptr << std::endl;
+    else
+        std::cerr << "Input is not of the types A, B or C." << std::endl;
 }
 
 void identify(Base &ref) {
-    if (dynamic_cast<A*>(&ref) != NULL)
-        std::cout << "Type is A" << std::endl;
-    else if (dynamic_cast<B*>(&ref) != NULL)
-        std::cout << "Type is B" << std::endl;
-    else if (dynamic_cast<C*>(&ref) != NULL)
-        std::cout << "Type is C" << std::endl;
+    try
+    {
+        A   &Aref = dynamic_cast<A &>(ref);
+        std::cout << "Type is A. Address: " << &Aref << std::endl;
+    }
+    catch(std::exception &e)
+    {
+        try
+        {
+            B   &Bref = dynamic_cast<B &>(ref);
+            std::cout << "Type is B. Address: " << &Bref << std::endl;
+        }
+        catch(std::exception &e)
+        {
+            try
+            {
+                C   &Cref = dynamic_cast<C &>(ref);
+                std::cout << "Type is C. Address: " << &Cref << std::endl;
+            }
+            catch(const std::exception& e)
+            {
+                std::cerr << e.what() << "Input is not of the types A, B or C." << std::endl;
+            }
+        }
+    }
+
 }
