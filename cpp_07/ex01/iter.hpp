9f135f95b8e139b0cdf8bb557fa76482756eb0f4
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-09 13:16:17 +0200

    Add working solution

--- cpp_07/ex01/iter.hpp
+++ cpp_07/ex01/iter.hpp
@@ -10,3 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ITER_HPP
+#define ITER_HPP
+
+template<typename T>
+void iter(T* array, int len, void (*function)(T &func))
+{
+    for (int i = 0; i < len; i++)
+        function(array[i]);
+}
+
+template<typename T>
+void print(T tem)
+{
+    std::cout << tem << std::endl;
+}
+
+template<typename T>
+void square(T &tem)
+{
+    tem*=tem;
+}
+
+#endif
--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -16,17 +16,39 @@
 int main() {
     std::string partition(30, '-');
 
+    char		chars[5] = {'a', 'b', 'c', 'd', 'e'};
+    int			ints[5] = {1, 2, 5, 7, 10};
+    char*		charptrs[5];
+    int*		intptrs[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        charptrs[i] = &chars[i];
+        intptrs[i] = &ints[i];
+    }
+
     std::cout << partition << std::endl;
-    std::cout << "// ... TEST //" << std::endl;
+    std::cout << "// CHAR TEST //" << std::endl;
     std::cout << partition << std::endl;
     {
+        iter(chars, 5, print);
+        std::cout << partition << std::endl;
 
+        iter(&charptrs, 5, print);
+        iter(charptrs, 5, print);
     }
     std::cout << partition << std::endl;
-    std::cout << "// ... TEST //" << std::endl;
+    std::cout << "// INT TEST //" << std::endl;
     std::cout << partition << std::endl;
     {
+        iter(ints, 5, print);
+        std::cout << partition << std::endl;
 
+        iter(ints, 5, square);
+        iter(ints, 5, print);
+        std::cout << partition << std::endl;
+
+        iter(intptrs, 5, print);
     }
     std::cout << partition << std::endl;
     return (0);
