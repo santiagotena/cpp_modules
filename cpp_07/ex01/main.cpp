1d55fe987927d6892e9a5498ab56421f8effec9d
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-09 11:53:10 +0200

    Add base files

---
+++ cpp_07/ex01/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/09 11:15:46 by stena-he          #+#    #+#             */
+/*   Updated: 2023/06/09 11:15:47 by stena-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "iter.hpp"
+
+int main() {
+    std::string partition(30, '-');
+
+    std::cout << partition << std::endl;
+    std::cout << "// ... TEST //" << std::endl;
+    std::cout << partition << std::endl;
+    {
+
+    }
+    std::cout << partition << std::endl;
+    std::cout << "// ... TEST //" << std::endl;
+    std::cout << partition << std::endl;
+    {
+
+    }
+    std::cout << partition << std::endl;
+    return (0);
+}
