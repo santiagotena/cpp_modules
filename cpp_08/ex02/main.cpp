83bd22fa19190b548aed0075b8247815af74bf57
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-13 14:24:40 +0200

    Add base files

---
+++ cpp_08/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 14:17:28 by stena-he          #+#    #+#             */
+/*   Updated: 2023/06/13 14:17:29 by stena-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main() {
+    std::string partition(30, '-');
+
+    std::cout << partition << std::endl;
+    std::cout << "// .. TESTS //" << std::endl;
+    std::cout << partition << std::endl;
+    {
+
+    }
+    std::cout << partition << std::endl;
+    std::cout << "// ... TESTS //" << std::endl;
+    std::cout << partition << std::endl;
+    {
+
+    }
+    std::cout << partition << std::endl;
+    return (0);
+}
